4643e69cde30a17613b1029282461e4963bcc6d7
vuflysocial <vufly.info@gmail.com>
2024-01-01 15:06:43 -0700

    finished

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,13 +41,13 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://raydium.io/swap/?inputCurrency=sol&outputCurrency=6gfob57g249n7SEKiJY9MNg5YipYezRQK1RzGcd7jJih&fixed=in"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               buy
             </Link>
             <Link
-              href="/"
+              href="https://www.dextools.io/app/en/solana/pair-explorer/5T8R4cphpfsnYgwqLTFg6MGFvWUagxHtRRyPDzATNYDQ"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
