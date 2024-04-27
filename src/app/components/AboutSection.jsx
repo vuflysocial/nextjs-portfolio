9471ca9f464deeee2a2afa2760cf0ae27c06b55b
vuflysocial <vufly.info@gmail.com>
2024-04-26 22:48:13 -0600

    up

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -36,22 +36,22 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          Welcome to 9sols.com, the pulsing heart of the Solana blockchain where cats don't just have nine lives—they also have nine sols! Meet $9Lives, your ticket to not just making a meme out of thin air but making a difference in the lives of our feline friends.
+  Welcome to 9sols.com, the pulsing heart of the Solana blockchain where cats don&apos;t just have nine lives—they also have nine sols! Meet $9Lives, your ticket to not just making a meme out of thin air but making a difference in the lives of our feline friends.
 
-Why are we here? To scratch behind the ears of convention and claw at the walls of the mundane. Launched by the purr-fectly capable hands of Rico Moneybags, a developer whose love for cats is only outmatched by his coding skills, 9Lives isn't just a project—it's a mission!
+  Why are we here? To scratch behind the ears of convention and claw at the walls of the mundane. Launched by the purr-fectly capable hands of Rico Moneybags, a developer whose love for cats is only outmatched by his coding skills, 9Lives isn&apos;t just a project—it&apos;s a mission!
 
-Join our crusade as we team up with TheCat (a.k.a Gato), a hero running a cat shelter in Chile, featured on Twitter as @morfeox23. With every transaction, with every meme shared, and with every sol we save, we're not just supporting a cause; we're building a community of cat lovers with a purpose.
+  Join our crusade as we team up with TheCat (a.k.a Gato), a hero running a cat shelter in Chile, featured on Twitter as @morfeox23. With every transaction, with every meme shared, and with every sol we save, we&apos;re not just supporting a cause; we&apos;re building a community of cat lovers with a purpose.
 
-What’s our goal? Simple: Save sols and souls. We believe in laughter, the power of memes, and the genuine good of supporting animal welfare. Under the watchful eyes of our trusted dev Rico Moneybags—find him @RicoMoneybags on Twitter and YouTube—we're not just building a platform; we're crafting a legacy.
+  What’s our goal? Simple: Save sols and souls. We believe in laughter, the power of memes, and the genuine good of supporting animal welfare. Under the watchful eyes of our trusted dev Rico Moneybags—find him @RicoMoneybags on Twitter and YouTube—we&apos;re not just building a platform; we&apos;re crafting a legacy.
 
-How to get involved?
+  How to get involved?
 
-Twitter: Follow us at @9solssolana for your daily dose of cat memes and project updates.
-Telegram: Join the 9 Lives Community here and be a part of the meowvement!
-Whether you're here to make profits or make a difference (or both!), at 9Lives, every little bit helps as we support not just any cats—your cats, stray cats, every cat.
+  Twitter: Follow us at @9solssolana for your daily dose of cat memes and project updates.
+  Telegram: Join the 9 Lives Community here and be a part of the meowvement!
+  Whether you&apos;re here to make profits or make a difference (or both!), at 9Lives, every little bit helps as we support not just any cats—your cats, stray cats, every cat.
 
-Remember: When you invest in $9Lives, you're not just investing in a project. You're investing in nine lives, nine sols, and one un-fur-gettable adventure. Let's make the internet purr, one meme at a time!
-          </p>
+  Remember: When you invest in $9Lives, you&apos;re not just investing in a project. You&apos;re investing in nine lives, nine sols, and one un-fur-gettable adventure. Let&apos;s make the internet purr, one meme at a time!
+</p>
           <div className="flex flex-row justify-start mt-8">
           
              
